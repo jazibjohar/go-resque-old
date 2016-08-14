ab129f2c3702146305402c82c574260e19a250b8
Baligh <jazib_johar@hotmail.com>
2016-08-14 18:05:19 +0500

    Changes

--- godis/driver.go
+++ godis/driver.go
@@ -65,38 +65,3 @@ func (d *drv) Poll() {
 		}
 	}(d)
 }
-
-func (d *drv) ListPushDelay(t time.Time, queue string, jobJSON string) (bool, error) {
-	_, err := d.client.Zadd(queue, timeToSecondsWithNanoPrecision(t), jobJSON)
-	if err != nil {
-		return false, err
-	}
-	if _, ok := d.schedule[queue]; !ok {
-		d.schedule[queue] = struct{}{}
-	}
-	return true, nil
-}
-func timeToSecondsWithNanoPrecision(t time.Time) float64 {
-	return float64(t.UnixNano()) / 1000000000.0 // nanoSecondPrecision
-}
-
-func (d *drv) Poll() {
-	go func(d *drv) {
-		for {
-			for key := range d.schedule {
-				now := timeToSecondsWithNanoPrecision(time.Now())
-				r, _ := d.client.Zrangebyscore(key, "-inf",
-					strconv.FormatFloat(now, 'E', -1, 64))
-				jobs := r.StringArray()
-				if len(jobs) == 0 {
-					continue
-				}
-				if removed, _ := d.client.Zrem(key, jobs[0]); removed {
-					queue := strings.TrimPrefix(key, d.nameSpace)
-					d.client.Lpush(d.nameSpace+"queue:"+queue, jobs[0])
-				}
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}(d)
-}
