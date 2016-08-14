28dbe7574164b1eb92291c49bdd3aa1273ddde27
Baligh <jazib_johar@hotmail.com>
2016-08-14 17:43:32 +0500

    Adding polling
    fixing changes

--- godis/driver.go
+++ godis/driver.go
@@ -28,7 +28,42 @@ func (d *drv) SetClient(name string, client interface{}) {
 }
 
 func (d *drv) ListPush(queue string, jobJSON string) (int64, error) {
-	return d.client.Rpush(queue, jobJSON)
+	return d.client.Rpush(d.nameSpace+"queue:"+queue, jobJSON)
+}
+
+func (d *drv) ListPushDelay(t time.Time, queue string, jobJSON string) (bool, error) {
+	_, err := d.client.Zadd(queue, timeToSecondsWithNanoPrecision(t), jobJSON)
+	if err != nil {
+		return false, err
+	}
+	if _, ok := d.schedule[queue]; !ok {
+		d.schedule[queue] = struct{}{}
+	}
+	return true, nil
+}
+func timeToSecondsWithNanoPrecision(t time.Time) float64 {
+	return float64(t.UnixNano()) / 1000000000.0 // nanoSecondPrecision
+}
+
+func (d *drv) Poll() {
+	go func(d *drv) {
+		for {
+			for key := range d.schedule {
+				now := timeToSecondsWithNanoPrecision(time.Now())
+				r, _ := d.client.Zrangebyscore(key, "-inf",
+					strconv.FormatFloat(now, 'E', -1, 64))
+				jobs := r.StringArray()
+				if len(jobs) == 0 {
+					continue
+				}
+				if removed, _ := d.client.Zrem(key, jobs[0]); removed {
+					queue := strings.TrimPrefix(key, d.nameSpace)
+					d.client.Lpush(d.nameSpace+"queue:"+queue, jobs[0])
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}(d)
 }
 
 func (d *drv) ListPushDelay(t time.Time, queue string, jobJSON string) (bool, error) {
